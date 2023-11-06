2e824248e9001f67b655a3c775da1723b862ad2c
Morgan <morgan.matprof@gmail.com>
2023-11-05 21:52:47 -0300

    quality probabilities added

--- GlobalQuality.cs
+++ GlobalQuality.cs
@@ -12,6 +12,7 @@ public class GlobalQuality : GlobalItem {
     public override bool InstancePerEntity => true;
     protected override bool CloneNewInstances => true;
     public static bool ArmorBroken = false;
+    public float ItemRoll;
     // Assign the custom field quality to *this* item (the actual instance of an item)
     public override void SetDefaults(Item item)
     {
@@ -20,6 +21,8 @@ public class GlobalQuality : GlobalItem {
         }
         
     }
+    // ================================================================================= //
+    #region ItemTooltips
     // Tooltips
     public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
     {
@@ -55,6 +58,48 @@ public class GlobalQuality : GlobalItem {
             }
         }       
     }
+    #endregion
+    // ============================================================================ //
+    #region Probability
+    public void RollQuality(Item item) {
+        ItemRoll = Main.rand.NextFloat();
+        if (ItemRoll <= 0.8f) {
+            quality = Main.rand.Next(30, 50+1); // 80% chance of getting item quality in 30-50 range, inclusive
+        }
+        if (ItemRoll <= 0.5f) {
+            quality = Main.rand.Next(51, 60+1); // 50% chance of getting item quality in 50-60 range, inclusive
+        }
+        if (ItemRoll <= 0.3f) {
+            quality = Main.rand.Next(61, 65+1); // 30% chance of getting item quality in 60-65 range, inclusive
+        }
+        if (ItemRoll <= 0.2f) {
+            quality = Main.rand.Next(66, 70+1); // 20% chance of getting item quality in 65-70 range, inclusive
+        }
+        if (ItemRoll <= 0.05f) {
+            quality = Main.rand.Next(71, 75+1); // 5% chance of getting item quality in 70-75 range, inclusive
+        }
+        if (ItemRoll <= 0.01f) {
+            quality = Main.rand.Next(76, 80+1); // 1% chance of getting item quality in 75-80 range, inclusive
+        }
+        if (ItemRoll <= 0.005f) {
+            quality = Main.rand.Next(81, 85+1); // 0.5% chance of getting item quality in 80-85 range, inclusive
+        }
+        if (ItemRoll <= 0.0001f) {
+            quality = Main.rand.Next(86, 90+1); // 0.01% chance of getting item quality in 85-90 range, inclusive
+        }
+        if (ItemRoll <= 0.00001f) {
+            quality = Main.rand.Next(91, 95+1); // 0.001% chance of getting item quality in 90-95 range, inclusive
+        }
+        if (ItemRoll <= 0.000001f) {
+            quality = Main.rand.Next(96, 99+1); // 0.0001% chance of getting item quality in 90-99 range, inclusive
+        }
+        if (ItemRoll <= 0.0000001f) {
+            quality = 100; // 0.00001% chance of getting item quality 100.
+        }
+    }
+    #endregion
+    // ============================================================================ //
+    #region ItemRestriction
     // Cannot use item if broken
     public override bool CanUseItem(Item item, Player player)
     {
@@ -93,19 +138,26 @@ public class GlobalQuality : GlobalItem {
             return true;
         } else return false;
     }
+    #endregion
+    // ====================================================================== //
+    #region ItemCreation
 
     // Assign random quality during item spawn (chests/drop/bags)
     public override void OnSpawn(Item item, IEntitySource source)
     {
-        quality = Main.rand.Next(0, 100+1);
+        RollQuality(item);
         //ArmorPenalty(item);
     }
     // Assign random quality during creation (craft)
     public override void OnCreate(Item item, ItemCreationContext context)
     {
-        quality = Main.rand.Next(0, 100+1);
+        RollQuality(item);
         //ArmorPenalty(item);
     }
+
+    #endregion
+    // ========================================================================= //
+    #region DataSave
     // Saving quality data with TagCompound object
     public override void SaveData(Item item, TagCompound tag)
     {
@@ -116,6 +168,6 @@ public class GlobalQuality : GlobalItem {
     {
         quality = tag.GetInt("quality");
     }
-
+    #endregion
 }
  
